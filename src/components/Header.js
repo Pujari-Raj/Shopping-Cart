d9d754bbbe9ac4c5d07e361d8d318fa27a53f04f
Pujari-Raj <basvarajpujari607@gmail.com>
2023-04-10 13:41:55 +0530

    done with navabr ui

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,36 @@
 import React from 'react'
-import { Container, Navbar } from 'react-bootstrap'
+import { Badge, Dropdown, Container, FormControl, Nav, Navbar } from 'react-bootstrap'
+import {FaShoppingCart} from 'react-icons/fa'
 
 const Header = () => {
-  return (
-    <Navbar bg='dark' variant='dark' style={{height: 80}}>
-        <Container>
-            <Navbar.Brand>
-                <a href='/'>Shopping Cart</a>
-            </Navbar.Brand>
-        </Container>
-    </Navbar>
-  )
+    return (
+        <Navbar bg='dark' variant='dark' style={{ height: 80 }}>
+            <Container>
+                <Navbar.Brand>
+                    <a href='/'>Shopping Cart</a>
+                </Navbar.Brand>
+                <Navbar.Text className='search'>
+                    <FormControl
+                        style={{ width: 500 }}
+                        placeholder='Search a Product'
+                        className='m-auto'
+                    />
+                </Navbar.Text>
+                <Nav>
+                    <Dropdown alignLeft>
+                        <Dropdown.Toggle variant='success'>
+                            <FaShoppingCart color='white' fontSize='25px' style={{marginRight: 5}} />
+                            <Badge>10</Badge>
+                        </Dropdown.Toggle>
+
+                        <Dropdown.Menu style={{ minWidth: 370 }}>
+                            <span style={{ padding: 10 }}>Cart Is Empty</span>
+                        </Dropdown.Menu>
+                    </Dropdown>
+                </Nav>
+            </Container>
+        </Navbar>
+    )
 }
 
 export default Header
