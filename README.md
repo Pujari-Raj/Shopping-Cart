476c99fb7ba6bf8681d6f7de47de5161c1b63c5b
Pujari Basvaraj <basvarajpujari607@gmail.com>
2023-08-18 23:12:11 +0530

    context api files

--- README.md
+++ README.md
@@ -9,4 +9,5 @@ TechStack
 Learnings from this projects:-
 * What is React-Router and How to use it.
 * How to Use React-Bootstrap.
+* What is react-redux, context api
 * How to add filters for different condition (Price, Ratings)
--- src/components/Filters.js
+++ src/components/Filters.js
@@ -27,6 +27,22 @@ const Filters = () => {
             checked={sort === "lowToHigh" ? true: false}
             />
         </span>
+        {/* <span>
+            <Form.Check 
+            inline
+            label='Descending'
+            name='group1'
+            type='radio'
+            id={`inline-2`}
+            onChange={() =>
+            productDispatch({
+                type: "SORT_BY_PRICE",
+                payload: "highToLow"
+            })
+            }
+            checked={sort === "highToLow" ? true: false}
+            />
+        </span> */}
         <span>
             <Form.Check 
             inline
